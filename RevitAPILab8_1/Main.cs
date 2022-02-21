d13bc0228426175c7df03f0e485103bcf2052a63
Sergei Khramtsov <vocmarh1989@gmail.com>
2022-02-22 00:07:41 +0500

    Добавьте файлы проекта.

---
+++ RevitAPILab8_1/Main.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPILab8_1
+{
+    [Transaction(TransactionMode.Manual)]
+
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Сообщение", "После закрытия данного окна произойдет экспорт файла проекта в формат IFC");
+
+
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            var ifcOption = new IFCExportOptions();
+            using (var ts = new Transaction(doc, "export ifc"))
+            {
+                ts.Start();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.ifc", ifcOption);
+                ts.Commit();
+            }
+
+            TaskDialog.Show("Сообщение", "Экспорт в формат IFC выполнен. Файл на Рабочем столе");
+            return Result.Succeeded;
+        }
+    }
+}
